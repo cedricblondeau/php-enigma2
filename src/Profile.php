67bf030413477ffe2ad0d5fb802c0aa1cc958eeb
Cedric Blondeau <cedric.blondeau.fr@gmail.com>
2015-10-28 12:04:19 -0400

    Profile class

---
+++ src/Profile.php
@@ -0,0 +1,71 @@
+<?php
+namespace CedricBlondeau\PhpEnigma2;
+
+class Profile
+{
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var string
+     */
+    private $user;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @param $host
+     * @param $user
+     * @param $password
+     */
+    function __construct($host, $user, $password)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * @param array $data
+     * @return Profile
+     * @throws \RuntimeException
+     */
+    public static function fromArray(array $data) {
+        if (isset($data['host']) && $data['host']
+            && isset($data['user']) && $data['user']) {
+            $profile = new self($data['host'], $data['user'], $data['password']);
+            return $profile;
+        } else {
+            throw new \RuntimeException("Invalid array");
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
