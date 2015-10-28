7ec5bea87f7ba684218b5dd1f3a2de654dbab735
Cedric Blondeau <cedric.blondeau.fr@gmail.com>
2015-10-28 12:09:27 -0400

    HTTP client (reload bouquets)

---
+++ src/Http/HttpClient.php
@@ -0,0 +1,42 @@
+<?php
+namespace CedricBlondeau\PhpEnigma2\Http;
+
+use CedricBlondeau\PhpEnigma2\Profile;
+
+class HttpClient
+{
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @param Profile $profile
+     */
+    function __construct(Profile $profile)
+    {
+        $this->url = "http://" . $profile->getHost() . "/web/";;
+    }
+
+    /**
+     * @param $path
+     * @return mixed
+     */
+    private function execute($path)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url . $path);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+    public function reloadBouquets()
+    {
+        $this->execute("servicelistreload?mode=0");
+    }
+}
